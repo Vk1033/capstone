1ff652806e45f1f6badcd6562b720d1442e94b75
Vk1033 <kinkarvansh@gmail.com>
2022-10-14 18:38:04 +0530

    Changes to be committed: 	modified:   src/App.jsx
    Changes not staged for commit:
    	modified:   package.json
    	modified:   yarn.lock
    
    Untracked files:
    	src/categories.styles.scss

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,23 @@
+import './category.styles.scss';
 function App() {
+  const categories = [
+    { id: 1, title: "Hats" },
+    { id: 2, title: "Jackets" },
+    { id: 3, title: "Sneakers" },
+    { id: 4, title: "Womens" },
+    { id: 5, title: "Mens" },
+  ];
   return (
-    <div>
-      <div className="categories-container">
+    <div className="categories-container">
+      {categories.map(({ title }) => (
         <div className="category-container">
-          {/* <img src="" alt="" /> */}
+          <div className="backgraoud-image" />
           <div className="category-body-container">
-            <h2>Hats</h2>
+            <h2>{title}</h2>
             <p>Shop Now</p>
           </div>
         </div>
-        <div className="category-container">
-          {/* <img src="" alt="" /> */}
-          <div className="category-body-container">
-            <h2>Jackets</h2>
-            <p>Shop Now</p>
-          </div>
-        </div>
-        <div className="category-container">
-          {/* <img src="" alt="" /> */}
-          <div className="category-body-container">
-            <h2>Sneakers</h2>
-            <p>Shop Now</p>
-          </div>
-        </div>
-        <div className="category-container">
-          {/* <img src="" alt="" /> */}
-          <div className="category-body-container">
-            <h2>Women</h2>
-            <p>Shop Now</p>
-          </div>
-        </div>
-        <div className="category-container">
-          {/* <img src="" alt="" /> */}
-          <div className="category-body-container">
-            <h2>Men</h2>
-            <p>Shop Now</p>
-          </div>
-        </div>
-      </div>
+      ))}
     </div>
   );
 }
