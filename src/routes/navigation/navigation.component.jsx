2d04820b0acb296491e40be65eaf7a0efe4c1d1a
Vk1033 <kinkarvansh@gmail.com>
2022-10-16 01:50:47 +0530

    fixed nav bar

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,26 +1,29 @@
 import { Fragment } from "react";
 import { Outlet, Link } from "react-router-dom";
+
 import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
+
 import "./navigation.styles.scss";
 
-const NavigationBar = () => {
+const Navigation = () => {
   return (
     <Fragment>
       <div className="navigation">
         <Link className="logo-container" to="/">
           <CrwnLogo className="logo" />
         </Link>
-        <div className="nav-links-container"></div>
-        <Link className="nav-link" to="/shop">
-          SHOP
-        </Link>
-        <Link className="nav-link" to="/auth">
-          Sign-in
-        </Link>
+        <div className="nav-links-container">
+          <Link className="nav-link" to="/shop">
+            SHOP
+          </Link>
+          <Link className="nav-link" to="/auth">
+            SIGN IN
+          </Link>
+        </div>
       </div>
       <Outlet />
     </Fragment>
   );
 };
 
-export default NavigationBar;
+export default Navigation;
