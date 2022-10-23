da7518706a39cb2f5a2493ff3d3cdb95e0c89b70
Vk1033 <kinkarvansh@gmail.com>
2022-10-24 00:04:49 +0530

    finished home page and finishing touches

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -9,13 +9,15 @@ import { useNavigate } from "react-router-dom";
 const CartDropdown = () => {
   const { cartItems } = useContext(CartContext);
   const navigate = useNavigate();
-  const goToCheckoutPage =()=> navigate('/checkout')
+  const goToCheckoutPage = () => navigate("/checkout");
   return (
     <div className="cart-dropdown-container">
       <div className="cart-items">
-        {cartItems.map((cartItem) => (
-          <CartItem key={cartItem.id} cartItem={cartItem} />
-        ))}
+        {cartItems.length ? (
+          cartItems.map((cartItem) => <CartItem key={cartItem.id} cartItem={cartItem} />)
+        ) : (
+          <span className="empty-message">Your cart is empty</span>
+        )}
       </div>
       <Button onClick={goToCheckoutPage}>GO TO CHECKOUT</Button>
     </div>
--- src/components/category-item/category-item.jsx
+++ src/components/category-item/category-item.jsx
@@ -1,10 +1,15 @@
 import "./category-item.styles.scss";
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const CategoryItem = ({ category }) => {
   const { title, imageUrl } = category;
+  const navigate = useNavigate();
+  const onNavigateHandler = () => {
+    navigate(`/shop/${title}`);
+  };
   return (
-    <div className="category-container">
+    <div className="category-container" onClick={onNavigateHandler}>
       <div className="background-image" style={{ backgroundImage: `url(${imageUrl})` }} />
       <div className="category-body-container">
         <h2>{title}</h2>
